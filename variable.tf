b7ee00874e18fc97d9a675aef5ccb55c452d961a
tor-wei <gohtorwei@gmail.com>
2025-07-26 21:51:12 +0800

    Fixed fmt issues

--- variable.tf
+++ variable.tf
@@ -1,19 +1,19 @@
-variable lambda_function_name {
-    description     = "Name of lambda function"
-    type            = string
-    default         = "torwei-package-scan-lambda-fn"
+variable "lambda_function_name" {
+  description = "Name of lambda function"
+  type        = string
+  default     = "torwei-package-scan-lambda-fn"
 }
 
-variable lambda_file_name {
-    description     = "Name of lambda file to be zipped"
-    type            = string
-    default         = "index"
+variable "lambda_file_name" {
+  description = "Name of lambda file to be zipped"
+  type        = string
+  default     = "index"
 }
 
 variable "iam_name" {
-    description = "Name of IAM"
-    type = string
-    default = "iam_package_scan_torwei_lambda"
+  description = "Name of IAM"
+  type        = string
+  default     = "iam_package_scan_torwei_lambda"
 }
 
 variable "vpc_name" {
@@ -23,7 +23,7 @@ variable "vpc_name" {
 }
 
 variable "created_by" {
-    description = "The name of vpc creator"
-    type        = string
-    default     = "torwei"
+  description = "The name of vpc creator"
+  type        = string
+  default     = "torwei"
 }
